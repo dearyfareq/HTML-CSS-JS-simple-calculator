c881432c2d3dcc6eda1543a4b8b0eaaf174f1087
Diyari <dearyfareq15@gmail.com>
2023-02-27 10:25:23 +0300

    added comments for future purposes

--- calulator.js
+++ calulator.js
@@ -1,28 +1,41 @@
+//Declare the variabels, one will display the results, one array for the buttons and one for the input
 let result = "";
 let screen = document.querySelector(".screen");
 let btn = document.querySelectorAll(".btn");
 
-console.log(screen)
-console.table(btn)
+//console.log(screen)
+//console.table(btn)
+
+//for each button in the array check for a click and then do the following:
 Array.from(btn).forEach((button) =>{
     button.addEventListener("click", () =>{
+        
+        // if it is = then evaluate the result then the input's value becomes the result
         if(button.innerHTML == "="){ 
             result = eval(result);
             screen.value = result;
         }
+        
+        // if it is C then clear everything then set that as the input value
         else if(button.innerHTML == "C"){
             result = "";
             screen.value = result;
         }
+
+        // if it is Del then slice or remove the last index as strings can be treated as arrays
         else if(button.innerHTML == "Del"){
             result = screen.value.slice(0,-1);
             screen.value = result;
             console.log(result)
         }
+
+        //anything else will be added to the value inside the input screen the evaluated
         else{
             result = result + button.innerHTML;
             screen.value = result;
         }
+
+        //without any conditions, change the color of every button on click then revert back after 100ms
         button.style.backgroundColor = "white";
         button.style.color = "#191825"
         setTimeout(() => 
